89db06c83df8877dc4eae2ad2d97995eabeb68f8
André Alves de Aguiar <51351450+AndreAlvesdeAguiar@users.noreply.github.com>
2024-05-15 17:09:25 -0300

    x

--- composables/date.ts
+++
@@ -1,7 +0,0 @@
-export const formatDate = (date: string, lang: string) => {
-  return new Date(date).toLocaleDateString(lang, {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-};
--- i18n/skills.js
+++
@@ -1,7 +0,0 @@
-export const skillsPTBR = {
-  certificateAluraDotNet: "Certificado de formação .NET da Alura",
-};
-
-export const skillsENUS = {
-  certificateAluraDotNet: ".NET training certificate from Alura",
-};
